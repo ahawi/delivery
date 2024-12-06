832f36ce55610a1b05c0f5ac33584ed64697c75c
ultrasue <nastya.murlla@gmail.com>
2024-12-06 23:08:05 +0300

    add script for modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 const modalFunc = () => {
-    const modal = document.querySelector()
-}
+  const modalElement = document.querySelector(".cart-modal__overlay");
+  const cartButton = document.querySelector("#cart");
+
+  const openModal = () => {
+    modalElement.classList.add("open");
+  };
+
+  const closeModal = () => {
+    modalElement.classList.remove("open");
+  };
+
+  cartButton.addEventListener("click", () => {
+    openModal();
+  });
+
+  modalElement.addEventListener("click", (event) => {
+    if (
+      event.target.classList.contains("cart-modal__overlay") ||
+      event.target.closest(".cart-modal__header-close")
+    ) {
+      closeModal();
+    }
+  });
+};
+
+modalFunc();
